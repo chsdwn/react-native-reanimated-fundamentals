b7df58306ecf5c67278d4f2bbfe3eb50a600f868
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-06-01 15:28:34 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+export const App = () => {
+  return (
+    <>
+      <StatusBar />
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+};
