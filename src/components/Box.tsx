7620be241baa18a2316ebd9608b9ea3a4937a93f
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-06-02 18:14:56 +0300

    Completed box opacity, touch to hide, release to visible.

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,25 +1,81 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, { Easing } from 'react-native-reanimated';
 import { State, TapGestureHandler } from 'react-native-gesture-handler';
 
-const { cond, eq, event, Value } = Animated;
+const {
+  and,
+  block,
+  cond,
+  eq,
+  event,
+  interpolate,
+  neq,
+  set,
+  startClock,
+  stopClock,
+  timing,
+  Clock,
+  Extrapolate,
+  Value,
+} = Animated;
+
+const runOpacityTimer = (clock: Clock, gestureState: any) => {
+  const state: Animated.TimingState = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0),
+  };
+
+  const config: Animated.TimingConfig = {
+    duration: 300,
+    toValue: new Value(-1),
+    easing: Easing.inOut(Easing.ease),
+  };
+
+  return block([
+    timing(clock, state, config),
+    cond(and(eq(gestureState, State.BEGAN), neq(config.toValue, 1)), [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.frameTime, 0),
+      set(config.toValue, 1),
+      startClock(clock),
+    ]),
+    cond(and(eq(gestureState, State.END), neq(config.toValue, 0)), [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.frameTime, 0),
+      set(config.toValue, 0),
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, stopClock(clock)),
+    interpolate(state.position, {
+      inputRange: [0, 1],
+      outputRange: [1, 0],
+      extrapolate: Extrapolate.CLAMP,
+    }),
+  ]);
+};
 
 export const Box = () => {
-  const state = new Value(-1);
-  const opacity = cond(eq(state, State.BEGAN), 0.2, 1);
+  const gestureState = new Value(-1);
+  const clock = new Clock();
+  const opacity = runOpacityTimer(clock, gestureState);
 
   const onStateChange = event([
     {
       nativeEvent: {
-        state,
+        state: gestureState,
       },
     },
   ]);
 
   return (
     <View style={styles.container}>
-      <TapGestureHandler onHandlerStateChange={onStateChange}>
+      <TapGestureHandler minDist={0} onHandlerStateChange={onStateChange}>
         <Animated.View style={[styles.box, { opacity }]}></Animated.View>
       </TapGestureHandler>
     </View>
